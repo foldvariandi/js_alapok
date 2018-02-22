246d44c67edef39f95bdbc2df3732584c731ec5c
Unknown <foldvari.andi16@gmail.com>
2018-02-22 13:17:20 +0100

    sdfsdf

---
+++ basic.js
@@ -0,0 +1,22 @@
+
+console.log("hello");
+(function () {
+'use strict'
+var szoveg2 = "valami";
+console.log(szoveg2);
+var szoveg2 = "valami";
+
+let szoveg3 = "szoveg3 ezt let-el hoztuk létre";
+console.log(szoveg3);
+//let szoveg3 = "szoveg3 ezt let-el hoztuk létre";
+
+szoveg3 = "uj erteket kap a szoveg3";
+console.log(szoveg3);
+
+    let szaml = 94;
+    console.log("a szaml valtozo érétke: "+szaml);
+    
+}());
+
+szoveg = "3";
+console.log("hello2");
